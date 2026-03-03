08d11ccb9b74265aa8a9d0cd80db00ac4ab22e26
Pavroz <fyriones@yandex.ru>
2026-03-03 18:34:44 +0500

    Добавление allure  в несколько тестов

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -9,38 +9,50 @@ logger = logging.getLogger(__name__)
 class TestAuth:
 
     @allure.story('Позитивные сценарии')
-    @allure.title('Проверка авторизации')
+    @allure.title('Проверка авторизации с корректными данными')
+    @pytest.mark.api
     @pytest.mark.auth
     def test_auth_correct(self, auth_endpoint):
         logger.info('Старт теста: проверка корректной авторизации')
-        response = auth_endpoint.auth_correct()
-        assert isinstance(response.text, str)  # Проверка, что ответ - строка
-        assert len(response.text) > 0  # Проверка, что длина ответа > 0
+        with allure.step('Отправка запроса на авторизацию'):
+            response = auth_endpoint.auth_correct()
+        with allure.step('Проверка тела ответа'):
+            assert isinstance(response.text, str)  # Проверка, что ответ - строка
+            assert len(response.text) > 0  # Проверка, что длина ответа > 0
         logger.info(f'Статус ответа: {response.status_code}')
-        assert response.status_code == 200  # Проверка, что код ответа 200
-        # return response.text
+        with allure.step('Проверка, что статус код 200'):
+            assert response.status_code == 200  # Проверка, что код ответа 200
+
 
     @allure.story('Негативные сценарии')
     @allure.title('Проверка авторизации с пустыми полями ввода')
     @pytest.mark.auth
     def test_auth_empty_login_and_password(self, auth_endpoint):
-        response = auth_endpoint.auth_empty_login_and_password()
-        assert response.status_code == 400
-        assert isinstance(response.json(), dict)
-        violations = response.json()['violations']
-        for v in violations:
-            assert v['message'] == "не должно быть пустым"
+        with allure.step('Отправка запроса на авторизацию'):
+            response = auth_endpoint.auth_empty_login_and_password()
+        with allure.step('Проверка, что статус кода 400'):
+            assert response.status_code == 400
+        with allure.step('Проверка тела ответа'):
+            assert isinstance(response.json(), dict)
+        with allure.step('Проверка сообщения об ошибке'):
+            violations = response.json()['violations']
+            for v in violations:
+                assert v['message'] == "не должно быть пустым"
 
     @allure.story('Негативные сценарии')
     @allure.title('Проверка авторизации с пустым логином')
     @pytest.mark.auth
     def test_auth_empty_login(self, auth_endpoint):
-        response = auth_endpoint.auth_empty_login()
-        assert isinstance(response.json(), dict)
-        assert response.status_code == 400
-        violations = response.json()['violations']
-        for v in violations:
-            assert v['message'] == "не должно быть пустым"
+        with allure.step('Отправка запроса на авторизацию'):
+            response = auth_endpoint.auth_empty_login()
+        with allure.step('Проверка тела ответа'):
+            assert isinstance(response.json(), dict)
+        with allure.step('Проверка, что статус кода 400'):
+            assert response.status_code == 400
+        with allure.step('Проверка сообщения об ошибке'):
+            violations = response.json()['violations']
+            for v in violations:
+                assert v['message'] == "не должно быть пустым"
 
     @allure.story('Негативные сценарии')
     @allure.title('Проверка авторизации с пустым паролем')
